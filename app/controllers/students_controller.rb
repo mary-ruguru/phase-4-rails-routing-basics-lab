0b51050078199a1686d71754d71b08737f933714
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2023-03-14 16:03:04 +0300

    completed assignment

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,17 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        students = Student.order(grade: :dsec)
+        render json: students
+    end
+
+    def highest_grade
+        students = Student.maximum(:grade)
+        students1 = Student.select("Max(grade) as max_grade").first.attributes
+        render json: students
+    end
+end
